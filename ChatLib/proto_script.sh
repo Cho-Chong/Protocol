2ad348192b8cb2466d11a4bd7575c7578d9ed175
Cho.Chong <Choyu89434@gmail.com>
2016-03-16 22:00:41 -0700

    Will migrate to use protobuf. Building library with 2.6.1

---
+++ ChatLib/proto_script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#  proto_script.sh
+#  ChatLib
+#
+#  Created by Cho Chong on 3/16/16.
+#  Copyright © 2016 Cho Chong. All rights reserved.
+
+echo "Generating protobuf files..."
+
+cd /Users/Cho/Documents/Programming/Chat/ChatLib/ChatLib
+./Google/protobuf/bin/protoc --proto_path=. records.proto --cpp_out=.
+
+echo "Built protobuf files"
+
+exit
